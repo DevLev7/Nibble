7b4b22d9d1eca4ddc06a23038b84ad35c8fa7140
Kate <katerinalevkina7@yandex.ru>
2020-10-18 23:12:58 +0300

    burger

---
+++ script/app.js
@@ -0,0 +1,21 @@
+// Открытие формы
+
+
+function openForm() {
+    document.getElementById("form_change").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("form_change").style.display = "none";
+  }
+
+function openBurger() {
+  document.getElementById("nav").style.display = "block"
+  document.getElementById("nav").style.display = "flex"
+  document.getElementById("auth").style.display = "block"
+  document.getElementById("auth").style.display = "flex"
+
+}  
+
+ 
+
