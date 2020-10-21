0e100ddb95eb26786c75678430907a2a3fc0e20c
Vanechek1999 <vanya.kulai@yandex.ru>
2020-10-21 23:22:20 +0300

    Footer and start PR

--- script/app.js
+++ script/app.js
@@ -4,18 +4,35 @@
 function openForm() {
     document.getElementById("form_change").style.display = "block";
   }
-  
+
   function closeForm() {
     document.getElementById("form_change").style.display = "none";
   }
 
-function openBurger() {
-  document.getElementById("nav").style.display = "block"
-  document.getElementById("nav").style.display = "flex"
-  document.getElementById("auth").style.display = "block"
-  document.getElementById("auth").style.display = "flex"
+  function openForm_signup() {
+    document.getElementById("form_signup").style.display = "flex";
+  }
 
-}  
 
- 
+
+
+
+ $(document).ready(function() {
+  $('.burger').click(function() {
+      $('.burger_item').toggleClass('open-menu');
+      $('.nav').toggleClass('open-menu');
+      $('.header-auth').toggleClass('open-menu');
+  });
+});
+
+
+ function openList1(){
+    document.getElementById("list1").style.display = "flex"
+}
+function openList2(){
+    document.getElementById("list2").style.display = "flex"
+}
+function openList3(){
+    document.getElementById("list3").style.display = "flex"
+}
 
