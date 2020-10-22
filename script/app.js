cd2bfdd5b4cb76555cb199146cc084a8e01f504a
Kate <katerinalevkina7@yandex.ru>
2020-10-22 22:31:31 +0300

    adaptetion

--- script/app.js
+++ script/app.js
@@ -2,29 +2,53 @@
 
 
 function openForm() {
-    document.getElementById("form_change").style.display = "block";
-  }
+  document.getElementById("form_change").style.display = "block";
+}
 
-  function closeForm() {
-    document.getElementById("form_change").style.display = "none";
-  }
+function closeForm() {
+  document.getElementById("form_change").style.display = "none";
+}
 
-  function openForm_signup() {
-    document.getElementById("form_signup").style.display = "flex";
-  }
+function openForm_signup() {
+  document.getElementById("form_signup").style.display = "flex";
+}
+
+function openForm_login() {
+  document.getElementById("form_login").style.display = "flex";
+  document.getElementById("form_reset").style.display = "none";
+}
+ 
 
 
+function openForm_forgotpass() {
+  document.getElementById("form_login").style.display = "none";
+  document.getElementById("form_forgotpass").style.display = "flex";
+}
+
+function openForm_reset() {
+  document.getElementById("form_forgotpass").style.display = "none";
+  document.getElementById("form_reset").style.display = "flex";
+}
 
 
-
- $(document).ready(function() {
-  $('.burger').click(function() {
-      $('.burger_item').toggleClass('open-menu');
-      $('.nav').toggleClass('open-menu');
-      $('.header-auth').toggleClass('open-menu');
-  });
+$(document).ready(function() {
+$('.burger').click(function() {
+    $('.burger_item').toggleClass('open-menu');
+    $('.nav').toggleClass('open-menu');
+    $('.header-auth').toggleClass('open-menu');
+});
 });
 
+$( '.nav' ).on("click", function(){
+  $('.nav').removeClass('open-menu');
+  $('.header-auth').removeClass('open-menu');
+});
+
+$( '.header-auth' ).on("click", function(){
+  $('.nav').removeClass('open-menu');
+  $('.header-auth').removeClass('open-menu');
+  
+});
 
  function openList1(){
     document.getElementById("list1").style.display = "flex"
